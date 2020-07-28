10f138660a6d194ccff3817ad5a21c5d9c710db5
myusername38 <martin38@live.unc.edu>
2020-07-28 10:00:10 -0400

    basics added

---
+++ functions/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBXyDne2ZcuhVg_ZXUxqcPl0rngNZy3Dak",
+    authDomain: "phonics-tutor.firebaseapp.com",
+    databaseURL: "https://phonics-tutor.firebaseio.com",
+    projectId: "phonics-tutor",
+    storageBucket: "phonics-tutor.appspot.com",
+    messagingSenderId: "417753181122",
+    appId: "1:417753181122:web:deb438c4e1fb00be36045b",
+    measurementId: "G-26NCLB2P8R"
+}
